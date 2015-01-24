13e8329b9dadd278b6d71cf70d01fcbe63290f21
John McLear <john@mclear.co.uk>
2015-01-24 02:53:45 +0000

    fixing export

--- index.js
+++ index.js
@@ -10,50 +10,21 @@ exports.eejsBlock_dd_format = function (hook_name, args, cb) {
   return cb();
 }
 
-function getInlineStyle(header) {
- // TODO!
-  switch (header) {
-  case "h1":
-    return "font-size: 2.0em;line-height: 120%;";
-    break;
-  case "h2":
-    return "font-size: 1.5em;line-height: 120%;";
-    break;
-  case "h3":
-    return "font-size: 1.17em;line-height: 120%;";
-    break;
-  case "h4":
-    return "line-height: 120%;";
-    break;
-  case "h5":
-    return "font-size: 0.83em;line-height: 120%;";
-    break;
-  case "h6":
-    return "font-size: 0.75em;line-height: 120%;";
-    break;
-  case "code":
-    return "font-family: monospace";
-  }
-  
-  return "";
-}
-// line, apool,attribLine,text
 exports.getLineHTMLForExport = function (hook, context) {
-  var header = _analyzeLine(context.attribLine, context.apool);
-  if (header) {
-    var inlineStyle = getInlineStyle(header);
-    return "<" + header + " style=\"" + inlineStyle + "\">" + context.text.substring(1) + "</" + header + ">";
+  var align = _analyzeLine(context.attribLine, context.apool);
+  if (align) {
+    return '<div style="width:100%;margin:0 auto;list-style-position:inside;text-align:' + align + '">' + context.lineContent + '</div>';
   }
 }
 
 function _analyzeLine(alineAttrs, apool) {
-  var header = null;
+  var align = null;
   if (alineAttrs) {
     var opIter = Changeset.opIterator(alineAttrs);
     if (opIter.hasNext()) {
       var op = opIter.next();
-      header = Changeset.opAttributeValue(op, 'align', apool);
+      align = Changeset.opAttributeValue(op, 'align', apool);
     }
   }
-  return header;
+  return align;
 }
