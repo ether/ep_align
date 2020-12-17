02f7ba620ce6ce251b9b34f8177714d91525a147
ilmar <ilmar.tyrk@gmail.com>
2020-12-17 13:20:05 +0200

    Fix html export with ep_headings

--- static/tests/backend/specs/exportHTML.js
+++ static/tests/backend/specs/exportHTML.js
@@ -112,7 +112,7 @@ describe('export alignment to HTML', function () {
             const html = res.body.data.html;
             const expectedHTML =
               '<p style=\'text-align:right\'>Hello world</p><br><br></body></html>';
-            if (html.indexOf(expectedHTML) === -1) throw new Error('No center tag detected');
+            if (html.indexOf(expectedHTML) === -1) throw new Error('No right tag detected');
           })
           .end(done);
     });
@@ -135,9 +135,40 @@ describe('export alignment to HTML', function () {
             const html = res.body.data.html;
             const expectedHTML =
               '<p style=\'text-align:left\'>Hello world</p><br><br></body></html>';
-            if (html.indexOf(expectedHTML) === -1) throw new Error('No center tag detected');
+            if (html.indexOf(expectedHTML) === -1) throw new Error('No left tag detected');
           })
           .end(done);
+    });
+  });
+
+  context('when pad text is heading', function () {
+    before(async function () {
+      html = () => buildHTML('<h1><left>Hello world</left></h1>');
+    });
+
+    it('returns ok', function (done) {
+      api.get(getHTMLEndPointFor(padID))
+          .expect('Content-Type', /json/)
+          .expect(200, done);
+    });
+
+    it('returns HTML with Subscript HTML tags', function (done) {
+      try {
+        require.resolve('ep_headings2'); // eslint-disable-line
+        api.get(getHTMLEndPointFor(padID))
+            .expect((res) => {
+              const html = res.body.data.html;
+              const expectedHTML =
+                '<h1 style=\'text-align:left\'>Hello world</h1><br><br></body></html>';
+              if (html.indexOf(expectedHTML) === -1) throw new Error('No left tag detected');
+            })
+            .end(done);
+      } catch (e) {
+        if (e.message.indexOf('Cannot find module') === -1) {
+          throw new Error(e.message);
+        }
+        done();
+      }
     });
   });
 });
